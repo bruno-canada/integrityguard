4d939a53ec845c98b5c32b2597718f61d3f46570
Bruno <55158870+bruno-canada@users.noreply.github.com>
2021-12-29 16:35:00 -0800

    Adjust load_config() method to use configparser and appdirs to load new .conf file

--- src/helpers/loadconfig.py
+++ src/helpers/loadconfig.py
@@ -1,11 +1,24 @@
-import yaml
+import configparser
+from appdirs import *
+import os
 
-def load_config(configPath=None):
+def load_config(config_path=None):
 
-    configPath = "./config.yml" if configPath is None else configPath
+    # Identify OS config default path
+    dirs = AppDirs("IntegrityGuard", "IntegrityGuard")
 
-    with open(configPath, "r") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            raise ValueError('Something went wrong loading the config.yml file. ' + exc)
+    # Define default config file path
+    config_file = os.path.join(dirs.user_config_dir, "integrityguard.conf")
+
+    # Check if the user provided a config path
+    if config_path != None:
+        config_file = os.path.join(config_path)
+
+    # Check if the config file exist
+    if os.path.exists(config_file) == False:
+        raise ValueError("The configuration file *" + config_file + "* doesn't exist.")
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    return config
