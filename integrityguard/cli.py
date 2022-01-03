92a6b76ac46046a65a6ca98573383e9f10660537
Bruno <55158870+bruno-canada@users.noreply.github.com>
2022-01-02 19:03:58 -0800

    Add support to --destination parameter to CLI

--- integrityguard/cli.py
+++ integrityguard/cli.py
@@ -2,6 +2,7 @@ import sys
 import click
 from integrityguard.helpers.loadconfig import load_config
 from integrityguard.helpers.showpaths import show_paths
+from integrityguard.helpers.copyconfig import copy_config
 from integrityguard.hashreport import hash_report
 from integrityguard.monitor import monitor
 
@@ -17,18 +18,36 @@ hash_type = config['hash']['hash_type'].lower()
 @click.command()
 @click.option('--task', default="monitor", help='Tasks available: monitor, generate_hashes')
 @click.option('--target', default=path, help='Target path to monitor')
-@click.option('--hash', default=hash_type, help='Hash algorithm type (MD5, SHA1, SHA224, SHA256, SHA384, and SHA512).')
+@click.option('--config', default="", help='Full path to .conf file')
+@click.option('--hash', default="", help='Hash algorithm type (MD5, SHA1, SHA224, SHA256, SHA384, and SHA512).')
+@click.option('--destination', default="", help='Used for copy_config task. Full destination path of the .conf file.')
 
-def main(task,target,hash):
+def main(task,target,config,hash,destination):
 
     """Console script for IntegrityGuard."""
 
     if task == "generate_hashes":
-        hash_report()
+        hash_report(config)
+
     elif task == "monitor":
-        monitor()
+        monitor(config)
+
+    elif task == "copy_config":
+        # Handle possible errors
+        if destination == "":
+            print("Error: --destination is required when using copy_config task.")
+            exit()
+
+        if destination.endswith('.conf') == False:
+            print("Error: --destination must be a full path including the file name. (e.g. <any-name>.conf)")
+            exit()
+
+        # Copy config file
+        copy_config(destination)
+
     elif task == "show_paths":
-        show_paths()
+        show_paths(config)
+
     return 0
 
 if __name__ == "__main__":
