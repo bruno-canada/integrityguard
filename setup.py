a65c21b1783be65386e63a9f7340583b184ba817
Bruno <55158870+bruno-canada@users.noreply.github.com>
2022-01-05 22:39:09 -0800

    Adjust version release to 0.4.3

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bruno-canada/integrityguard',
-    version='0.4.2',
+    version='0.4.3',
     zip_safe=False,
 )
 
